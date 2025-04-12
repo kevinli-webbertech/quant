66eea4b265ab1f80646b99c910a7035d4f772891
Kevin Li <xiaofeng.li2@va.gov>
2025-04-11 22:59:39 -0400

    complete a script

--- asset_tracking/start.sh
+++ asset_tracking/start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 cd backend
+echo "Python3 installing packages..."
 pip3 install -r requirements.txt
 python3 create_db.py
 nohup python3 app.py &
 sleep 3
 curl http://localhost:5000/api/health
+cd ../frontend
+echo "NPM installing packages..."
+npm install
+nohup npm start &
